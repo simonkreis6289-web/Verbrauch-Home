fd2e721b0117a9503a62b1b8e40d43f7dc692402
simonkreis6289-web <simonkreis6289@gmail.com>
2026-04-20 09:36:51 +0200

    Refactor service worker event listeners syntax

--- sw.js
+++ sw.js
@@ -8,18 +8,14 @@ const urlsToCache = [
   "./icons/icon-512.png"
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+    caches.match(event.request).then((response) => response || fetch(event.request))
   );
 });
