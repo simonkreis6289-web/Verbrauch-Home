a4c320bc261f9abc7dfc96b024fc5331cbe48f67
simonkreis6289-web <simonkreis6289@gmail.com>
2026-04-20 08:43:34 +0200

    Refactor service worker caching logic

--- sw.js
+++ sw.js
@@ -1,28 +1,25 @@
-const CACHE_NAME = "verbrauch-zuhause-cache-v1";
-const APP_FILES = [
+const CACHE_NAME = "verbrauch-app-v1";
+
+const urlsToCache = [
   "./",
   "./index.html",
-  "./styles.css",
-  "./app.js",
   "./manifest.webmanifest",
   "./icons/icon-192.png",
   "./icons/icon-512.png"
 ];
 
-self.addEventListener("install", (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_FILES)));
-});
-
-self.addEventListener("activate", (event) => {
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
-    )
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then((cached) => cached || fetch(event.request))
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
   );
 });
