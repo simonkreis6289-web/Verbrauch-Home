5ee714ab9c7c312f2f66ccdac695cd6d61ba7ded
simonkreis6289-web <simonkreis6289@gmail.com>
2026-04-30 12:53:31 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,6 +8,33 @@ const urlsToCache = [
   "./icon-512.png"
 ];
 
+importScripts("https://www.gstatic.com/firebasejs/10.12.4/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.12.4/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBrKjajSNPgmFUwjMghkox1kM8NjbCWZ_Y",
+  authDomain: "verbrauch-zuhause.firebaseapp.com",
+  projectId: "verbrauch-zuhause",
+  storageBucket: "verbrauch-zuhause.firebasestorage.app",
+  messagingSenderId: "546121139332",
+  appId: "1:546121139332:web:b07b58184fb0388565aed1",
+  measurementId: "G-L0GQQ8V785"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  const title = payload?.notification?.title || "Verbrauch Zuhause";
+  const body = payload?.notification?.body || "Neue Daten eingetragen.";
+
+  self.registration.showNotification(title, {
+    body,
+    icon: "./icon-192.png",
+    badge: "./icon-192.png",
+    data: payload?.data || {}
+  });
+});
+
 self.addEventListener("install", (event) => {
   self.skipWaiting();
   event.waitUntil(
